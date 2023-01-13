bef8cf177dc33dd0c493b6098829559cf6857a38
Maksim Levanovich <maxlevanovich@gmail.com>
2023-01-12 19:32:48 -0600

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -15,13 +18,13 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       });
     try {
     } catch (err) {
       console.log(err);
     }
-    setWord("");
+    setWord('');
   };
   return (
     <div>
